0cf8e2c2e6ae9f55652cb74e3e259d34476e78c7
Mike <scharfmn@gmail.com>
2016-06-30 10:20:34 +0530

    tidy pass

--- web/demo/views.py
+++ web/demo/views.py
@@ -3,29 +3,28 @@ from flask import render_template, request, redirect, make_response
 from web.demo import demo
 
 
-@demo.route('/post', methods=['POST'])
+@demo.route('/post', methods=['POST']) # TODO: move to client
 def _post():
     if request.method == 'POST':
-        data = dict(request.form)
+        data = request.form
         total = 0
         choices = []
         for key in data:
-            if key.startswith('n') and data[key][0]:
-                if int(data[key][0]) > 0:
-                    choices.append((int(key.split('n')[1]), int(data[key][0])))
-                    total += int(data[key][0])
-        resp = make_response(redirect('/confirm'))
+            if key.startswith('n') and data[key] and int(data[key]) > 0:
+                choices.append((int(key.split('n')[1]), int(data[key])))
+                total += int(data[key])
+        resp = make_response(redirect('/siat'))
         resp.set_cookie('choices', json.dumps(choices))
         resp.set_cookie('total', json.dumps({'total':total}))
         return resp
 
-@demo.route('/confirm')
+@demo.route('/siat')
 def _peit():
     choices = json.loads(request.cookies.get('choices'))
     total = json.loads(request.cookies.get('total'))
     return render_template('confirm.html', choices=choices, total=total)
 
-@demo.route('/choose')
+@demo.route('/peit')
 def _siat():
-    x = range(1,101)[::5]
-    return render_template('choose.html', x=x)
+    rows = range(1,101)[::5]
+    return render_template('choose.html', rows=rows)
